370fd6a02ff50966a63ca9c4274b9e575d32def3
Jason Barnett <jason.w.barnett@gmail.com>
2016-07-02 09:14:56 -0400

    move files too

--- convert.sh
+++ convert.sh
@@ -3,7 +3,12 @@ egrep -Rl '"fb"' .  | egrep -v '\.git|convert\.sh' | xargs sed -i 's|"fb"|"bw"|g
 egrep -Rl 'fb_' .   | egrep -v '\.git|convert\.sh' | xargs sed -i 's|fb_|bw_|g'
 egrep -Rl 'fb\.' .  | egrep -v '\.git|convert\.sh' | xargs sed -i 's|fb\.|bw.|g'
 
-for dir in `find . -type d -name '*fb*'`; do
+for dir in `find . -type d -name '*fb*' | egrep -v '\.git'`; do
   new_name=$(echo $dir | sed 's|fb_|bw_|g')
   mv $dir $new_name
+done
+
+for file in `find . -type f -name '*fb*' | egrep -v '\.git'`; do
+  new_name=$(echo $file | sed 's|fb|bw|g')
+  mv $file $new_name
 done
