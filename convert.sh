8ef4bc37a6a74856a287929cbf6e5ba3a00e3f54
Jason Barnett <jason.w.barnett@gmail.com>
2016-07-02 09:12:50 -0400

    move cookbook directories

--- convert.sh
+++ convert.sh
@@ -3,3 +3,7 @@ egrep -Rl '"fb"' .  | egrep -v '\.git|convert\.sh' | xargs sed -i 's|"fb"|"bw"|g
 egrep -Rl 'fb_' .   | egrep -v '\.git|convert\.sh' | xargs sed -i 's|fb_|bw_|g'
 egrep -Rl 'fb\.' .  | egrep -v '\.git|convert\.sh' | xargs sed -i 's|fb\.|bw.|g'
 
+for dir in `find . -type d -name '*fb*'`; do
+  new_name=$(echo $dir | sed 's|fb_|bw_|g')
+  mv $dir $new_name
+done
