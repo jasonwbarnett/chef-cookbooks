adbca5b3882fa0f2b0d195b4b50d389da75fe44c
Jason Barnett <jason.w.barnett@gmail.com>
2016-07-02 09:10:48 -0400

    added fb. replacement

--- convert.sh
+++ convert.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-egrep -Rl '"fb"' . | egrep -v '\.git|convert\.sh' | xargs sed -i 's|"fb"|"bw"|g'
-egrep -Rl 'fb_' .  | egrep -v '\.git|convert\.sh' | xargs sed -i 's|fb_|bw_|g'
+egrep -Rl '"fb"' .  | egrep -v '\.git|convert\.sh' | xargs sed -i 's|"fb"|"bw"|g'
+egrep -Rl 'fb_' .   | egrep -v '\.git|convert\.sh' | xargs sed -i 's|fb_|bw_|g'
+egrep -Rl 'fb\.' .  | egrep -v '\.git|convert\.sh' | xargs sed -i 's|fb\.|bw.|g'
 
